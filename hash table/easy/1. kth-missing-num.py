e620865fe040c3431552847735b25904e747953d
sdas13 <s.das001@yahoo.com>
2020-04-13 19:31:28 +0530

    kth missing number

---
+++ hash table/easy/1. kth-missing-num.py
@@ -0,0 +1,10 @@
+"""
+Given an array arr of positive integers sorted in a strictly increasing order, and an integer k. Find the kth positive integer that is missing from this array.
+
+Input: arr = [2,3,4,7,11], k = 5
+Output: 9 The missing positive integers are [1,5,6,8,9,10,12,13,...]. The 5th missing positive integer is 9.
+
+Input: arr = [1,2,3,4], k = 2
+Output: 6 The missing positive integers are [5,6,7,...]. The 2nd missing positive integer is 6.
+"""
+
