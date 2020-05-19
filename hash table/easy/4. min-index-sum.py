c0f4aac2d034790ae4d2cda3642fedc9474ebd34
sdas13 <s.das001@yahoo.com>
2020-05-19 20:04:16 +0530

    min index sum

---
+++ hash table/easy/4. min-index-sum.py
@@ -0,0 +1,15 @@
+"""
+find out their common interest with the least list index sum
+
+Input: list1 = ["Shogun","Tapioca Express","Burger King","KFC"], list2 = ["Piatti","The Grill at Torrey Pines","Hungry Hunter Steakhouse","Shogun"]
+Output: ["Shogun"]
+
+Input: list1 = ["Shogun","Tapioca Express","Burger King","KFC"], list2 = ["KFC","Shogun","Burger King"]
+Output: ["Shogun"] index sum is "Shogun" with index sum 1 (0+1).
+
+Input: list1 = ["Shogun","Tapioca Express","Burger King","KFC"], list2 = ["KFC","Burger King","Tapioca Express","Shogun"]
+Output: ["KFC","Burger King","Tapioca Express","Shogun"]
+
+Input: list1 = ["Shogun","Tapioca Express","Burger King","KFC"], list2 = ["KNN","KFC","Burger King","Tapioca Express","Shogun"]
+Output: ["KFC","Burger King","Tapioca Express","Shogun"]
+"""
