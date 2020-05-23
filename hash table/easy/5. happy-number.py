d5f2ff1a68dc4ec65c6d2a3367babedc342da5a2
sdas13 <s.das001@yahoo.com>
2020-05-23 21:59:01 +0530

    happy number

--- hash table/easy/5. happy-number.py
+++ hash table/easy/5. happy-number.py
@@ -4,6 +4,8 @@ Starting with any positive integer, replace the number by the sum of the squares
 equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process
 ends in 1 are happy.
 
+https://leetcode.com/problems/happy-number/discuss/56917/My-solution-in-C(-O(1)-space-and-no-magic-math-property-involved-)
+
 Input: n = 19
 Output: true
 Explanation:12 + 92 = 82, 82 + 22 = 68, 62 + 82 = 100, 12 + 02 + 02 = 1
