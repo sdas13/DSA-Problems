219c0b8047b5338a1faf57a86b8b27803bd4409b
sdas13 <s.das001@yahoo.com>
2020-06-10 22:44:56 +0530

    set mismatch

---
+++ hash table/easy/7. set-mismatch.py
@@ -0,0 +1,11 @@
+"""
+In set S, one of the numbers in the set got duplicated to another number in the set, which results in repetition of one number
+and loss of another number.find the number occurs twice and then find the number that is missing.
+Return them in the form of an array
+
+XoR: https://leetcode.com/problems/set-mismatch/solution/
+https://leetcode.com/problems/set-mismatch/discuss/105558/Oneliner-Python
+
+Input: nums = [1,2,2,4]
+Output: [2,3]
+"""
