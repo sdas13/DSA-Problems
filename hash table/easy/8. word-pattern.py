e6bb2cd4a4b2e8ef1534d726e5ffcceb437acc73
sdas13 <s.das001@yahoo.com>
2020-06-16 20:55:55 +0530

    word pattern

---
+++ hash table/easy/8. word-pattern.py
@@ -0,0 +1,17 @@
+"""
+Given a pattern and a string s, find if s follows the same pattern. Here follow means a full match, such that there is a
+bijection between a letter in pattern and a non-empty word in s.
+
+https://leetcode.com/problems/word-pattern/solution/
+
+Input: pattern = "abba", s = "dog cat cat dog" Output: true
+Input: pattern = "abba", s = "dog cat cat fish" Output: false
+Input: pattern = "aaaa", s = "dog cat cat dog" Output: false
+"""
+
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        pass
+
+
+
