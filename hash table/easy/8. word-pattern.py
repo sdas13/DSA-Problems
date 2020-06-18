d31f777696c08927aae44ce3586da42d8e27db04
sdas13 <s.das001@yahoo.com>
2020-06-18 20:56:35 +0530

    word pattern

--- hash table/easy/8. word-pattern.py
+++ hash table/easy/8. word-pattern.py
@@ -11,7 +11,22 @@ Input: pattern = "aaaa", s = "dog cat cat dog" Output: false
 
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        pass
+        # instead of storing a:b and b:a, store a:0, b:0
+        mapping1 = {}
+        mapping2 = {}
+
+        s = s.split()
+
+        if len(s) != len(pattern):
+            return False
+
+        for i, j in enumerate(pattern):
+            if (j in mapping1 and mapping1[j] != s[i]) or (s[i] in mapping2 and mapping2[s[i]] != j):
+                return False
+            mapping1[j] = s[i]
+            mapping2[s[i]] = j
+
+        return True
 
 
 
