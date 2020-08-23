5f1c687762511643eaaaf3746b22af2e4178666e
sdas13 <s.das001@yahoo.com>
2020-08-23 22:50:07 +0530

    first unique char

---
+++ hash table/easy/2. first-unique-char.py
@@ -0,0 +1,13 @@
+"""
+Given a string, find the first non-repeating character in it and return its index. If it doesn't exist, return -1.
+
+faster: https://leetcode.com/problems/first-unique-character-in-a-string/discuss/86351/Python-3-lines-beats-100-(~-60ms)-!
+
+s = "leetcode"
+return 0.
+
+s = "loveleetcode"
+return 2.
+
+"""
+
