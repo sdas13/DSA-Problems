412839c670734cec9c9723e11aac03dddcb65825
sdas13 <s.das001@yahoo.com>
2020-08-23 22:55:33 +0530

    first unique char

--- hash table/easy/2. first-unique-char.py
+++ hash table/easy/2. first-unique-char.py
@@ -14,6 +14,13 @@ return 2.
 
 class Solution:
     def firstUniqChar(self, s: str) -> int:
+        counter = {}
+        for i in s:
+            counter[i] = counter.get(i, 0) + 1
+
+        for i in range(len(s)):
+            if counter[s[i]] == 1:
+                return i
 
         return -1
 
