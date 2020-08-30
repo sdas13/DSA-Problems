25838c6b17bbb68e2586bb5f7f7b1ee5d3bc3ad9
sdas13 <s.das001@yahoo.com>
2020-08-30 22:18:03 +0530

    kth missing number

--- hash table/easy/1.* kth-missing-num.py
+++ hash table/easy/1.* kth-missing-num.py
@@ -1,6 +1,9 @@
 """
 Given an array arr of positive integers sorted in a strictly increasing order, and an integer k. Find the kth positive integer that is missing from this array.
 
+binary-search: https://leetcode.com/problems/kth-missing-positive-number/discuss/779999/JavaC%2B%2BPython-O(logN)
+my improvised solution: https://leetcode.com/problems/kth-missing-positive-number/discuss/780114/Java-Simple-Solution-Explained-in-detail100-Faster
+
 Input: arr = [2,3,4,7,11], k = 5
 Output: 9 The missing positive integers are [1,5,6,8,9,10,12,13,...]. The 5th missing positive integer is 9.
 
