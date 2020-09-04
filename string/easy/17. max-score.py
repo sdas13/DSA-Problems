8e5a9f79535249a32a3177d8afc2676ead69459b
sdas13 <s.das001@yahoo.com>
2020-09-05 00:27:08 +0530

    max-score

--- string/easy/17. max-score.py
+++ string/easy/17. max-score.py
@@ -2,6 +2,8 @@
 Given a string s of zeros and ones, return the maximum score after splitting the string into two non-empty substrings (i.e. left substring and right substring)
 The score after splitting a string is the number of zeros in the left substring plus the number of ones in the right substring.
 
+https://leetcode.com/problems/maximum-score-after-splitting-a-string/discuss/597740/Java-StraightForward
+
 Input: s = "011101"
 Output: 5 Explanation: All possible ways of splitting s into two non-empty substrings are:
 left = "0" and right = "11101", score = 1 + 4 = 5 left = "01" and right = "1101", score = 1 + 3 = 4 left = "011" and right = "101", score = 1 + 2 = 3
@@ -15,6 +17,7 @@ Output: 5
 class Solution:
     def maxScore(self, s: str) -> int:
 
+        # no need of map
         if s[0] == '0':
             count_at_each_indx = {0: [1, 0]}
         else:
@@ -38,6 +41,8 @@ class Solution:
                 count_at_each_indx[i] = [count_at_each_indx[i][0], count_at_each_indx[i + 1][1]]
 
         total = 0
+
+        #3 passes can be reduced to 2 passes by doing below comparison in the above loop
 
         for i in range(1, len(s)):
             total = max(total, count_at_each_indx[i - 1][0] + count_at_each_indx[i][1])
