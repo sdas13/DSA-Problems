4f28cd7a92b0331fe22aa4ec24c4081035541958
sdas13 <s.das001@yahoo.com>
2020-09-08 23:28:12 +0530

    greatest common divisor - py

---
+++ string/easy/15. greatest-common-divisor.py
@@ -0,0 +1,12 @@
+"""
+Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2. we say "t divides s" if and only if s = t + ... + t  (t concatenated with itself 1 or more times)
+
+Input: str1 = "ABCABC", str2 = "ABC"
+Output: "ABC"
+
+Input: str1 = "ABABAB", str2 = "ABAB"
+Output: "AB"
+
+Input: str1 = "ABCDEF", str2 = "ABC"
+Output: ""
+"""
