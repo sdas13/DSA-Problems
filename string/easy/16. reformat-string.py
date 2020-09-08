01bb8746f17e9565ca744e5c7d538bbf5baa4746
sdas13 <s.das001@yahoo.com>
2020-09-09 00:13:04 +0530

    reformat-string

--- string/easy/16. reformat-string.py
+++ string/easy/16. reformat-string.py
@@ -1,3 +1,6 @@
 """
+You have to find a permutation of the string where no letter is followed by another letter and no digit is followed by another digit. That is, no two adjacent characters have the same type.
+Return the reformatted string or return an empty string if it is impossible to reformat the string.
+
 
 """
