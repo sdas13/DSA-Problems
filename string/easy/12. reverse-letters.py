eca64180d4e37ce85ad62e9e8e03165b4383d893
sdas13 <s.das001@yahoo.com>
2020-09-23 23:42:14 +0530

    reverse letter - python

---
+++ string/easy/12. reverse-letters.py
@@ -0,0 +1,34 @@
+"""
+Given a string S, return the "reversed" string where all characters that are not a letter stay in the same place, and all letters reverse their positions.
+
+Input: "a-bC-dEf-ghIj"
+Output: "j-Ih-gfE-dCba"
+
+Input: "Test1ng-Leet=code-Q!"
+Output: "Qedo1ct-eeLg=ntse-T!"
+"""
+
+
+class Solution:
+    def reverseOnlyLetters(self, S: str) -> str:
+        i = 0
+        j = len(S) - 1
+
+        S = list(S)
+
+        while i < j:
+            if not S[i].isalpha():
+                i += 1
+            elif not S[j].isalpha():
+                j -= 1
+            else:
+                S[i], S[j] = S[j], S[i]
+                i += 1
+                j -= 1
+
+        return ''.join(S)
+
+
+# print(Solution().reverseOnlyLetters('ab-cd'))
+# print(Solution().reverseOnlyLetters('a-bC-dEf-ghIj'))
+print(Solution().reverseOnlyLetters('Test1ng-Leet=code-Q!'))
