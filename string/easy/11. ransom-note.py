3d4662dd370bedcaafef5477f7436289fa454ba0
sdas13 <s.das001@yahoo.com>
2020-10-09 22:09:48 +0530

    ransom note - python

--- string/easy/11. ransom-note.py
+++ string/easy/11. ransom-note.py
@@ -2,6 +2,9 @@
 Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
 Each letter in the magazine string can only be used once in your ransom note.
 
+https://leetcode.com/problems/ransom-note/discuss/85757/pythonset()count()
+https://leetcode.com/problems/ransom-note/discuss/85837/O(m%2Bn)-one-liner-Python
+
 Input: ransomNote = "a", magazine = "b"
 Output: false
 
