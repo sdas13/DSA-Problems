af1b9f5b4e4d019aabd7ca756edb72051fad89ab
sdas13 <s.das001@yahoo.com>
2020-10-14 21:34:14 +0530

    detect capital - python

--- string/easy/10. detect-capital.py
+++ string/easy/10. detect-capital.py
@@ -5,6 +5,8 @@ All letters in this word are capitals, like "USA".
 All letters in this word are not capitals, like "leetcode".
 Only the first letter in this word is capital, like "Google".
 
+https://leetcode.com/problems/detect-capital/discuss/99248/3-Lines
+
 Input: "USA"
 Output: True
 
@@ -15,7 +17,7 @@ Output: False
 
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
-        return word.capitalize() == word or word.isupper() or word.islower()
+        return word.capitalize() == word or word.isupper() or word.islower() # istitle()
 
 
 print(Solution().detectCapitalUse('USA'))
