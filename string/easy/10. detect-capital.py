1b1890819dea4a44cca4c8d3f882852ee39da367
sdas13 <s.das001@yahoo.com>
2020-10-14 20:59:48 +0530

    detect capital - python

---
+++ string/easy/10. detect-capital.py
@@ -0,0 +1,20 @@
+"""
+Given a word, you need to judge whether the usage of capitals in it is right or not: like
+
+All letters in this word are capitals, like "USA".
+All letters in this word are not capitals, like "leetcode".
+Only the first letter in this word is capital, like "Google".
+
+Input: "USA"
+Output: True
+
+Input: "FlaG"
+Output: False
+"""
+
+
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        pass
+
+print(Solution().detectCapitalUse('USA'))
