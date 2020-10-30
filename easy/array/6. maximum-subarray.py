f835f0d1020828b3445443e2dca7b9c2f2430907
sdas13 <s.das001@yahoo.com>
2020-10-31 01:03:45 +0530

    maximum subarrat - python

--- easy/array/6. maximum-subarray.py
+++ easy/array/6. maximum-subarray.py
@@ -1,6 +1,8 @@
 """
 Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum
 
+# kadane algorithm
+
 Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
 Output: 6
 
@@ -11,19 +13,21 @@ Output: -2147483647
 
 class Solution:
     def maxSubArray(self, nums: list) -> int:
-        if len(nums) == 1:
-            return nums[0]
+        i = 1
+        max_till_now = nums[0]
+        local_max = nums[0]
+        while i < len(nums):
+            current = nums[i]
+            local_max = max(current, local_max + current)
+            max_till_now = max(max_till_now, local_max)
+            i += 1
+
+        return max_till_now
 
 
-
-
-
+# print(Solution().maxSubArray([-1]))
 # print(Solution().maxSubArray([-2, 1]))
 # print(Solution().maxSubArray([-2, 1, 2]))
 # print(Solution().maxSubArray([-2, 1, 2, 3]))
-
-
-print(Solution().maxSubArray([-2, 1, -3, 4]))
-# print(Solution().maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])
-
-
+# print(Solution().maxSubArray([-2, 1, -3, 4]))
+print(Solution().maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
