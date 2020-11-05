a534aab01bb2679386b349edd8b644c553142a6d
sdas13 <s.das001@yahoo.com>
2020-11-05 20:10:53 +0530

    intersection of two arrays 2 - python

---
+++ easy/array/17.* two-sum-2.py
@@ -0,0 +1,22 @@
+"""
+Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+
+The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.
+
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+
+Input: numbers = [2,3,4], target = 6
+Output: [1,3]
+
+Input: numbers = [-1,0], target = -1
+Output: [1,2]
+"""
+
+
+class Solution:
+    def twoSum(self, numbers: list, target: int) -> list:
+        pass
+
+
+print(Solution().twoSum([2, 7, 8, 10, 11, 15], 9))
