9f6b06451e994355d22b133965b41e137f98dbcf
sdas13 <s.das001@yahoo.com>
2020-11-06 23:49:36 +0530

    degree of array - python

--- easy/array/20. degree-of-array.py
+++ easy/array/20. degree-of-array.py
@@ -2,6 +2,8 @@
 Given a non-empty array of non-negative integers nums, the degree of this array is defined as the maximum frequency of any one of its elements.
 Your task is to find the smallest possible length of a (contiguous) subarray of nums, that has the same degree as nums.
 
+https://leetcode.com/problems/degree-of-an-array/discuss/124317/JavaC++Python-One-Pass-Solution
+
 Input: nums = [1,2,2,3,1]
 Output: 2
 Explanation: The input array has a degree of 2 because both elements 1 and 2 appear twice.
