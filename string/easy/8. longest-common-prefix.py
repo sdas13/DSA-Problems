7f3e52e68e4220ee1d175521330aa0e8206d337a
sdas13 <s.das001@yahoo.com>
2020-12-05 22:53:03 +0530

    longest common prefix - python

--- string/easy/8. longest-common-prefix.py
+++ string/easy/8. longest-common-prefix.py
@@ -7,3 +7,34 @@ Output: "fl"
 Input: strs = ["dog","racecar","car"]
 Output: ""
 """
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: list) -> str:
+        if not strs: return ""
+
+        common_str = strs[0]
+
+        for i in range(1, len(strs)):
+
+            j = 0
+            temp = []
+            while j < min(len(common_str), len(strs[i])):
+                if common_str[j] == strs[i][j]:
+                    temp.append(common_str[j])
+                else:
+                    break
+                j += 1
+            if not temp: return ""
+
+            common_str = ''.join(temp)
+
+        return common_str
+
+
+# print(Solution().longestCommonPrefix(["flower", "flow", "flight"]))
+# print(Solution().longestCommonPrefix(["flower", "f", "flight"]))
+# print(Solution().longestCommonPrefix(["f", "flow", "flight"]))
+# print(Solution().longestCommonPrefix(["flower", "x", "flight"]))
+# print(Solution().longestCommonPrefix(["flower", "flight", "x"]))
+print(Solution().longestCommonPrefix(["dog","racecar","car"]))
