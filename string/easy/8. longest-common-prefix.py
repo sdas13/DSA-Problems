8a0f8eb04cd6e8344729a77ebcaab6e94e7ad3aa
sdas13 <s.das001@yahoo.com>
2020-12-05 19:45:47 +0530

    Input: strs = [dog,racecar,car]

---
+++ string/easy/8. longest-common-prefix.py
@@ -0,0 +1,9 @@
+"""
+Write a function to find the longest common prefix string amongst an array of strings. If there is no common prefix, return an empty string ""
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+"""
