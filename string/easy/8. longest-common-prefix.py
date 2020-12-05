ae417b07f256a198ccc9b214c81aa364d7bf91a5
sdas13 <s.das001@yahoo.com>
2020-12-06 01:29:15 +0530

    longest common prefix - python

--- string/easy/8. longest-common-prefix.py
+++ string/easy/8. longest-common-prefix.py
@@ -1,6 +1,8 @@
 """
 Write a function to find the longest common prefix string amongst an array of strings. If there is no common prefix, return an empty string ""
 
+https://leetcode.com/problems/longest-common-prefix/solution/
+
 Input: strs = ["flower","flow","flight"]
 Output: "fl"
 
@@ -18,23 +20,23 @@ class Solution:
         for i in range(1, len(strs)):
 
             j = 0
-            temp = []
-            while j < min(len(common_str), len(strs[i])):
-                if common_str[j] == strs[i][j]:
-                    temp.append(common_str[j])
-                else:
-                    break
-                j += 1
-            if not temp: return ""
 
-            common_str = ''.join(temp)
+            while j < min(len(common_str), len(strs[i])):
+                if common_str[j] != strs[i][j]:
+                    break
+
+                j += 1
+
+            common_str = common_str[:j]
 
         return common_str
 
 
+# print(Solution().longestCommonPrefix(["flower", "flow"]))
 # print(Solution().longestCommonPrefix(["flower", "flow", "flight"]))
 # print(Solution().longestCommonPrefix(["flower", "f", "flight"]))
 # print(Solution().longestCommonPrefix(["f", "flow", "flight"]))
 # print(Solution().longestCommonPrefix(["flower", "x", "flight"]))
+# print(Solution().longestCommonPrefix(["baab","bacb","b","cbc"]))
 # print(Solution().longestCommonPrefix(["flower", "flight", "x"]))
-print(Solution().longestCommonPrefix(["dog","racecar","car"]))
+print(Solution().longestCommonPrefix(["dog", "racecar", "car"]))
